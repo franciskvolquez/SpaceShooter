c12f5dede86068ac1f93868839af79c28dcc9f8d
Francisco Martinez <franciskvolquez@hotmail.com>
2017-02-10 01:06:32 -0400

    Create bolts

---
+++ Assets/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Sets the velocity in the positive z axis. 
+        rb.velocity = transform.forward * speed;
+    }
+}
